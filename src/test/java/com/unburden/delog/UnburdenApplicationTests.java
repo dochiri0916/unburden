a9a6598bb62c033c0c94efeafb6f135582579d26
송성빈 <dochiri0916@gmail.com>
2026-02-10 23:30:23 +0900

    build: Spring Boot 4 프로젝트 초기 세팅

---
+++ src/test/java/com/unburden/delog/UnburdenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unburden.delog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnburdenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
